99f72caa2cc5b423ae854a83f49c9c92e1247d2f
Quốc Hào <quochao170402@gmail.com>
2024-02-29 22:26:12 +0700

    fix bug env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,16 +5,22 @@ import { SequelizeModule } from '@nestjs/sequelize';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { StockModule } from './stock/stock.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env'],
+      isGlobal: true,
+      cache: true,
+    }),
     SequelizeModule.forRoot({
       dialect: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'test',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT) || 3306,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       models: [],
       autoLoadModels: true,
       synchronize: true,

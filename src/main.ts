4e47864c01d396c7508af0dec8e5d1a856419b81
ThuongDoo <domanhthuong20122002@egmail.com>
2024-03-01 19:35:46 +0700

    update cors

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
   app.use(passport.session());
   app.setGlobalPrefix('api/v1');
   app.enableCors({
-    origin: ['http://localhost:3001', 'http://localhost:3002'], // Đổi thành domain của trang web gốc của bạn
+    origin: '*', 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });

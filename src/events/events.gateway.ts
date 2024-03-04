5ca5abd03f9f4626daa1029b716faeee0d226edf
ThuongDoo <domanhthuong20122002@egmail.com>
2024-03-04 14:46:46 +0700

    socket

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -9,16 +9,16 @@ import {
 import {} from '@nestjs/platform-socket.io';
 import { Server, Socket } from 'socket.io';
 import { StockService } from 'src/stock/stock.service';
-import { Inject, forwardRef } from '@nestjs/common';
+import { Inject, Logger, forwardRef } from '@nestjs/common';
 
 @WebSocketGateway({
   cors: {
     origin: '*',
     methods: ['GET', 'POST'],
     transports: ['websocket', 'polling'],
-    credentials: true,
+    // credentials: true,
   },
-  allowEIO3: true,
+  // allowEIO3: true,
 })
 export class EventsGateway
   implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
@@ -26,6 +26,8 @@ export class EventsGateway
   constructor(
     @Inject(forwardRef(() => StockService)) private stockService: StockService,
   ) {}
+
+  private logger: Logger = new Logger('AppGateway');
   @WebSocketServer() server: Server;
   @SubscribeMessage('updateStockRequest')
   async handleUpdateStock(client: Socket, payload: any) {
@@ -45,6 +47,7 @@ export class EventsGateway
   }
   handleConnection(client: Socket, ...args: any[]) {
     // delete
+    this.logger.log(`Client connected: ${client.id}`);
     console.log(args);
 
     console.log('Connected', client.id);

add878793f2b7724a046ae2be6659458b3e88e37
ThuongDoo <domanhthuong20122002@gmail.com>
2024-06-24 14:08:55 +0700

    change role guard

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,22 +1,26 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { format } from 'date-fns';
+import { Public } from './shared/decorator/public.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+  @Public()
   @Get('cpu-usage')
   getCpuUsage(): any {
     const usage = process.cpuUsage();
     return { usage };
   }
 
+  @Public()
   @Get('memory-usage')
   getMemoryUsage(): any {
     const usage = process.memoryUsage();
     return usage;
   }
 
+  @Public()
   @Get('time')
   getCurrentDateTime(): string {
     const currentDateTime = new Date();
